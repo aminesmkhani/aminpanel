03adaaacd933d5f413cc4d470bbdb6bb6a2fc831
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-04 20:41:45 +0330

    Config Persian Font-Bakh!

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      body: ['Bakh'],
+      display:['Bakh'],
+     },
     extend: {},
   },
   variants: {
